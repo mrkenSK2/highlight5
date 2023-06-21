dfc50ab13f9cc0616fc258ca74bec12893775abb
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2023-06-21 22:33:22 +0900

    書き換えのみ

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-const input = document.querySelector('input[type="search"]');
+const before = 'o';
+const after = '8月32日'; // beforeの部分文字列から構成してもいいかも
 
-input.addEventListener("search", () => {
-    document.getElementById('h1').value = "content"
-});
+// HTMLノードを再帰的に探索して文字列置換をする関数
+const recursiveReplace = (nodes) => {
+  nodes.forEach(node => {
+    // 子ノードがあったらその配列を引数にした自身を実行
+    if (node.hasChildNodes()) recursiveReplace(node.childNodes);
+    // テキストノードで中身があればreplaceを試みる
+    if (node.nodeType === 3 && node.textContent) {
+      const replaced = node.textContent.replace(before, after); // ここで8月延長
+      node.textContent = replaced;
+    }
+  });
+}
 
-submit.addEventListener('click', function () {
-	document.getElementById('h1') = "content"
+// ロード完了してから以下を実行
+window.addEventListener('load', () => {
+  // 静的HTML書き換え
+  // body以下のscript以外のノードの中身をざっくりreplaceしてみる
+  const childNodes = document.querySelectorAll('html body :not(script)');
+  for (let i = 0; i < childNodes.length; i++) {
+    const replaced = childNodes[i].innerHTML.replace(before, after); // ここで8月延長
+    childNodes[i].innerHTML = replaced;
+  }
+
+  // 動的HTML書き換え
+  // まずはObserverでDOM変化があった場合のコールバックを定義
+  const observer = new MutationObserver(records => {
+    // 追加を検知したノードの配列を再帰的文字列置換の関数に入れて実行する
+    records.forEach(record => recursiveReplace(record.addedNodes));
+  });
+  // body全体から下のノード全てを監視対象にする
+  observer.observe(document.querySelector('html body'), {
+    childList: true,
+    subtree: true,
+  });
 });
 /*let search = document.querySelector(`input[name='search']`);
 //let submit = document.querySelector(`input[name='submit']`);
