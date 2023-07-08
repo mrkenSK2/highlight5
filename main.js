5516928f2b3257f0aa4408477189cb24e96db2a6
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2023-07-08 14:40:10 +0900

    change id index

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 for (let i = 1; i <= 5; i++) {
     let search = document.getElementById(`typed${i}`);
-    let submit = document.querySelector(`input[name='submit${i}']`);
     search.addEventListener('change', function () {
         chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             chrome.tabs.sendMessage(
                 tabs[0].id,
                 {
                     val: search.value,
-                    id: i
+                    id: i - 1
                 },
                 function(response) {
                     console.log(response);
